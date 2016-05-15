910c64bc3d0caa0d1c0a7d9938842c71cd9a4be1
Matteo Filippi <info@mfilippi.guru>
2016-05-15 17:27:16 +0200

    fix folder hierarchy

--- work/aswproject/projects/snapcook/app.js
+++ work/aswproject/projects/snapcook/app.js
@@ -19,5 +19,5 @@ app.use(function (req, res, next){
 app.use("/cookbook", cookBookController);
 app.use("/recipe", recipeController);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/app'));
 app.listen(process.env.PORT || config.express.port);
